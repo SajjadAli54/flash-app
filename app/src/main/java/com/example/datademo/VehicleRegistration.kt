849a9f645b8c2c8dfc1395dcf08e71900884932a
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-12-11 00:19:37 +0500

    Nothing

--- app/src/main/java/com/example/datademo/VehicleRegistration.kt
+++ app/src/main/java/com/example/datademo/VehicleRegistration.kt
@@ -2,10 +2,16 @@ package com.example.datademo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class VehicleRegistration : AppCompatActivity() {
+    var color = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vehicle_registration)
+    }
+
+    fun handleColor(view: View){
+
     }
 }
