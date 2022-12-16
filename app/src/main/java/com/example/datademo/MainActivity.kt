025afd97736fb7f43592ececa266498cfef8ef2c
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-12-16 23:42:21 +0500

    Developed dialog box for role

--- app/src/main/java/com/example/datademo/MainActivity.kt
+++ app/src/main/java/com/example/datademo/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.datademo
 
 import android.content.Intent
-import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.datademo.data.DatabaseHandler
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +18,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         databaseHandler = DatabaseHandler.getSingleton(this)
-
     }
 
     fun login(v: View){
@@ -30,26 +30,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loginPassenger(username: String, password: String){
-        if(databaseHandler.existsPassenger(username, password)){
+        if(databaseHandler.existsPassenger(username, password))
             Toast.makeText(this, "I am registered as passenger", Toast.LENGTH_SHORT).show()
-        }
-        else{
+        else
             Toast.makeText(this, "I am not registered as passenger", Toast.LENGTH_SHORT).show()
-        }
     }
 
-
     private fun loginDriver(username: String, password: String){
-        if(databaseHandler.existsDriver(username, password)){
+        if(databaseHandler.existsDriver(username, password))
             Toast.makeText(this, "I am registered as Driver", Toast.LENGTH_SHORT).show()
-        }
-        else{
+        else
             Toast.makeText(this, "I am not registered as Driver", Toast.LENGTH_SHORT).show()
-        }
     }
 
     fun register(v:View){
-        val intent : Intent = Intent(this, Role::class.java)
+        val dialogBuilder = AlertDialog.Builder(this)
+        val view = layoutInflater.inflate(R.layout.role_account, null)
+        dialogBuilder.setView(view)
+
+        dialogBuilder.setTitle("Select Role")
+        val alertDialog: AlertDialog = dialogBuilder.create()
+        alertDialog.show()
+
+        val button = view.findViewById<Button>(R.id.nextButton)
+
+        button.setOnClickListener {
+            val driverRB = view.findViewById<RadioButton>(R.id.driverRB)
+            if(driverRB.isChecked)
+                callRegisterForm("Driver")
+            else
+                callRegisterForm("Passenger")
+            alertDialog.hide()
+        }
+    }
+
+    private fun callRegisterForm(role : String){
+        val intent = Intent(this, PassengerRegistration::class.java)
+        intent.putExtra("role", role)
         startActivity(intent)
     }
 }
