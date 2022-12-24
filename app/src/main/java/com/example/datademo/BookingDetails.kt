e616caa0d80ed88519e0b9cda37835e3786f5fea
shankar-01 <shankar.bscsf19@iba-suk.edu.pk>
2022-12-24 12:32:09 +0500

    google payement method integrated

--- app/src/main/java/com/example/datademo/BookingDetails.kt
+++ app/src/main/java/com/example/datademo/BookingDetails.kt
@@ -1,11 +1,20 @@
 package com.example.datademo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class BookingDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking_details)
+    }
+    fun checkOut(v: View){
+        val intent = Intent(this, CheckoutActivity::class.java)
+        startActivity(intent)
+    }
+    fun cancel(v: View){
+        finish()
     }
 }
